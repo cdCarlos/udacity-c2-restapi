6c95aadd9881410f5bda264a132896b683441e7c
Carlos Daniel <cd.carlosx2@gmail.com>
2019-06-27 21:15:52 -0600

    Fix sequelize constructor to support custom port

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,18 +1,16 @@
-import { Sequelize } from 'sequelize-typescript';
-import { config } from './config/config';
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+import { SequelizeConfig } from "sequelize-typescript/lib/types/SequelizeConfig";
 
 const c = config.pg;
 
-// Instantiate new Sequelize instance!
-export const sequelize = new Sequelize({
-  "username": c.username,
-  "password": c.password,
-  "database": c.database,
-  "host": c.host,
-  "port": c.port,
-
+const sequelize_config: SequelizeConfig = {
+  username: c.username,
+  password: c.password,
+  database: c.database,
+  host: c.host,
+  port: +c.port,
   dialect: c.dialect,
-  storage: ':memory:',
-});
-
+  storage: ":memory:"
+};
+export const sequelize = new Sequelize(sequelize_config);
