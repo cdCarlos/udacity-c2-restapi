89a5c322f7413856a7a22e80f4e8c7c7a1265c64
Carlos Daniel <cd.carlosx2@gmail.com>
2019-06-27 22:24:26 -0600

    Complement users endpoints

--- src/controllers/v0/users/routes/user.router.ts
+++ src/controllers/v0/users/routes/user.router.ts
@@ -1,20 +1,45 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 
-import { User } from '../models/User';
-import { AuthRouter, requireAuth } from './auth.router';
+import { User } from "../models/User";
+import { AuthRouter, requireAuth } from "./auth.router";
 
 const router: Router = Router();
 
-router.use('/auth', AuthRouter);
+router.use("/auth", AuthRouter);
 
-router.get('/', async (req: Request, res: Response) => {
-    res.send('not implemented!!')
+router.get("/", async (req: Request, res: Response) => {
+  const users = await User.findAndCountAll();
+  res.send(users);
 });
 
-router.get('/:id', async (req: Request, res: Response) => {
-    let { id } = req.params;
-    const item = await User.findByPk(id);
-    res.send(item);
+router.get("/:id", async (req: Request, res: Response) => {
+  let { id } = req.params;
+  const item = await User.findByPk(id);
+  res.send(item);
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // check email is valid
+  if (!email) {
+    return res.status(400).send({ message: "Email is required or malformed" });
+  }
+
+  // check password is valid
+  if (!password) {
+    return res.status(400).send({ message: "Password is required" });
+  }
+
+  const user = await new User({
+    email: email,
+    password_hash: password
+  });
+
+  const saved_user = await user.save();
+
+  res.status(201).send(saved_user);
 });
 
 export const UserRouter: Router = router;
