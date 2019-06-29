48ed1c7476888172c171dbe6cfd7a7d0e899af71
Carlos Daniel <cd.carlosx2@gmail.com>
2019-06-29 12:42:32 -0600

    Format

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,18 @@
 export const config = {
-  "pg": {
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
-    "dialect": process.env.DB_DIALECT,
-  },
+    pg: {
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        dialect: process.env.DB_DIALECT
+    },
+    aws: {
+        aws_region: process.env.AWS_REGION,
+        aws_profile: process.env.AWS_PROFILE,
+        aws_media_bucket: process.env.AWS_MEDIA_BUCKET
+    },
     jwt: {
         secret: process.env.JTW_SECRET
-}
+    }
 };
