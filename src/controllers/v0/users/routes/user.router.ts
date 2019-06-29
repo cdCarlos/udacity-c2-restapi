0afdf8e856e51309c280121a5a8dec9b04eaf497
Carlos Daniel <cd.carlosx2@gmail.com>
2019-06-29 13:00:19 -0600

    Remove post user creation since it is in auth

--- src/controllers/v0/users/routes/user.router.ts
+++ src/controllers/v0/users/routes/user.router.ts
@@ -18,40 +18,4 @@ router.get("/:id", async (req: Request, res: Response) => {
     res.send(item);
 });
 
-router.post("/", async (req: Request, res: Response) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    // check email is valid
-    if (!email) {
-        return res
-            .status(400)
-            .send({ message: "Email is required or malformed" });
-    }
-
-    // check password is valid
-    if (!password) {
-        return res.status(400).send({ message: "Password is required" });
-    }
-
-    // check if user already exists
-    if (await User.findByPk(email)) {
-        res.status(422).send({
-            auth: false,
-            message: "This user may already exists"
-        });
-    }
-
-    const hashed_password = await generatePassword(password);
-
-    const user = await new User({
-        email: email,
-        password_hash: hashed_password
-    });
-
-    const saved_user = await user.save();
-
-    res.status(201).send(saved_user);
-});
-
 export const UserRouter: Router = router;
