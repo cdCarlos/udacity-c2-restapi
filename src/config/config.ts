d8e1c7173f25ce450b82038e8af76e1e10f2bacb
Carlos Daniel <cd.carlosx2@gmail.com>
2019-06-29 12:42:05 -0600

    Add jwt generation

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,9 +7,7 @@ export const config = {
     "port": process.env.POSTGRES_PORT,
     "dialect": process.env.DB_DIALECT,
   },
-  "aws": {
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
-  }
+    jwt: {
+        secret: process.env.JTW_SECRET
 }
+};
